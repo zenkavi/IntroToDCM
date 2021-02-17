754ce43d0313c1d80121f1f8c4b6b7b9ff88e48a
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-02-17 14:08:07 -0800

    cont modifying demo

--- ThreeNodeNetwork.m
+++ ThreeNodeNetwork.m
@@ -4,19 +4,39 @@
 
 % DEM Structure: create random inputs
 % -------------------------------------------------------------------------
-T  = 512;                             % number of observations (scans)
+T  = 100;                             % number of observations (scans)
 TR = 2;                               % repetition time or timing
 t  = (1:T)*TR;                        % observation times
 n  = 3;                               % number of regions or nodes
 u  = spm_rand_mar(T,n,1/2)/4;         % endogenous fluctuations
 % spm_rand_mar creates an AR(1) process with autoregression coefficient set
-% at 1/2 and the standard deviation of the fluctuations are scaled to 1/4
+% at 1/2 and the standard deviation of the fluctuations are scaled to 1/4.
+% This is supposed to represent resting state activity
+
+% Plot endogenous fluctuations
+subplot(1,2,1)
+plot(t, u)
+ylim([-1 1.5])
+xlim([0 T*TR])
+title('Endogenous fluctuations')
+
 
 % experimental inputs (Cu = 0 to suppress)
 % -------------------------------------------------------------------------
 % Cu  = [1; 0; 0] * 0;                  % no input for resting state
 Cu  = [1; 0; 0] * 1;                  % input at node 1
-E   = cos(2*pi*TR*(1:T)/24) * 0;
+% E   = cos(2*pi*TR*(1:T)/24) * 0;
+on_len = 10;
+off_len = 5;
+num_blocks = T/(on_len + 2*off_len);
+E   = repmat([repelem(0, off_len) repelem(1, on_len) repelem(0, off_len)], 1, num_blocks);
+
+% Plot experimental input
+subplot(1,2,2)
+plot(t, E)
+ylim([-1 1.5])
+xlim([0 T*TR])
+title('Experimental input')
 
 % priors
 % -------------------------------------------------------------------------
@@ -41,7 +61,7 @@ pP.A = [  0  .2    0;
 pP.C = eye(n,n);
 pP.transit = randn(n,1)/16;
 
-% simulate response to endogenous fluctuations
+% simulate response to endogenous fluctuations + experimental input
 %==========================================================================
 
 % integrate states
@@ -55,57 +75,3 @@ U.dt = TR;
 % Make hidden states
 x    = spm_int_J(pP,M,U);
 
-% haemodynamic observer
-% -------------------------------------------------------------------------
-
-% Convert hidden states into fmri timeseries
-for i = 1:T
-y(i,:) = spm_gx_fmri(spm_unvec(x(i,:),M.x),[],pP)';
-end
-
-% observation noise process
-% -------------------------------------------------------------------------
-e    = spm_rand_mar(T,n,1/2)/4;
-
-% show simulated response
-%--------------------------------------------------------------------------
-i = 1:T;
-spm_figure('Getwin','Figure 1'); clf
-subplot(2,2,1)
-plot(t(i),u(i,:))
-title('Endogenous fluctuations','FontSize',16)
-xlabel('Time (seconds)')
-ylabel('Amplitude')
-axis square
-
-subplot(2,2,2), hold off
-plot(t(i),x(i,n + 1:end),'c'), hold on
-plot(t(i),x(i,1:n)), hold off
-title('Hidden states','FontSize',16)
-xlabel('Time (seconds)')
-ylabel('Amplitude')
-axis square
-
-subplot(2,2,3)
-plot(t(i),y(i,:),t(i),e(i,:),':')
-title('Hemodynamic response and noise','FontSize',16)
-xlabel('Time (seconds)')
-ylabel('Amplitude')
-axis square
-
-
-% nonlinear system identification (DCM for CSD)
-%==========================================================================
-DCM.options = options;
-
-DCM.a    = logical(pP.A);
-DCM.b    = zeros(n,n,0);
-DCM.c    = logical(Cu);
-DCM.d    = zeros(n,n,0);
-
-% response
-% -------------------------------------------------------------------------
-DCM.Y.y  = y + e;
-DCM.Y.dt = TR;
-DCM.U.u  = E';
-DCM.U.dt = TR;
