4d8e62c48ef113af0cd2c1e08c1c7b8bfb742266
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-02-23 18:42:36 -0800

    cont building template

--- ThreeNodeNetwork.m
+++ ThreeNodeNetwork.m
@@ -5,12 +5,44 @@
 
 % Read Smith et al. (2011) to understand why this has 2714 samples
 org_DCM = load('tapas-master/rDCM/test/DCM_LargeScaleSmith_model1.mat')
-org_DCM = org_DCM.DCM
+org_DCM = org_DCM.DCM;
 
-my_DCM
+% specify the options for the rDCM analysis
+options.SNR             = 3;
+options.y_dt            = 0.5;
+options.p0_all          = 0.15;  % single p0 value (for computational efficiency)
+options.iter            = 100;
+options.filter_str      = 5;
+options.restrictInputs  = 1;
 
+% run a simulation (synthetic) analysis
+type = 's';
 
-%% Effects of the FFT
+% create DCM structure based on the fields in the org_DCM
+% add fields needed by tapas_rdcm_generate.m
+
+% Input
+my_DCM.U.name =
+my_DCM.U.u =
+my_DCM.U.dt =
+
+% True parameters
+my_DCM.Tp.A = 
+my_DCM.Tp.B = 
+my_DCM.Tp.C = 
+my_DCM.Tp.D = []
+my_DCM.Tp.transit = 
+my_DCM.Tp.decay = 
+my_DCM.Tp.epsilon = 
+
+% A priori connectivity matrix (set to full connectivity to see if it can
+% correctly prune it to the true matrix)
+nr = 3
+my_DCM.a = ones(nr, nr)
+
+DCM = tapas_rdcm_generate(my_DCM, options, options.SNR);
+
+%% Effect of the FFT
 
 % What is the effect of the Fast Fourier transform used in rDCM?
 
